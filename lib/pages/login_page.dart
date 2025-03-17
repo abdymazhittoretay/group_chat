76074d6ae095082fbdfc69ed0778de3da0fecd15
Toretay <abdymvzhit@gmail.com>
2025-03-17 15:35:48 +0500

    added appbar

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("Login Page"), centerTitle: true),
+    );
   }
 }
