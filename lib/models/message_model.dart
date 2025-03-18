2f24cf158b794be91c325df9ae7d10aea376e6bc
Toretay <abdymvzhit@gmail.com>
2025-03-18 14:35:34 +0500

    added message_model

---
+++ lib/models/message_model.dart
@@ -0,0 +1,11 @@
+class MessageModel {
+  final String message;
+  final String sentBy;
+  final DateTime date;
+
+  MessageModel({
+    required this.message,
+    required this.sentBy,
+    required this.date,
+  });
+}
